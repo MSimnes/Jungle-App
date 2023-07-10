7fc5ca2bb75b565c64ed481c8cd0afd3daf4844f
Mike <mike.simnes@gmail.com>
2023-07-10 14:10:10 -0700

    Add authenticate and validate logic.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,23 @@
 class User < ApplicationRecord
 
-  has_secure_password # This method comes from bcrypt gem
+  has_secure_password
+  
+  # add authentication logic here
+  def self.authenticate_with_credentials(email, password)
+    email = email.strip.downcase
+    user = User.find_by_email(email)
+    if user && user.authenticate(password)
+      user
+    else
+      nil
+    end
+  end
+
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  # validates :email, presence: true, uniqueness: { case_sensitive: false }
+  validates :email, presence: true, uniqueness: true
+  validates :password, presence: true, length: { minimum: 3 }
+  validates :password_confirmation, presence: true
 
 end
