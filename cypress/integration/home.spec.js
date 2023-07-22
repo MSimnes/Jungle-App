0c5dd7f0dcd887a3f7e79551c7be2cdb2826e84d
Mike <mike.simnes@gmail.com>
2023-07-21 21:01:16 -0700

    Add tests for product page.

---
+++ cypress/integration/home.spec.js
@@ -0,0 +1,14 @@
+describe('localhost:3000', () => {
+  beforeEach(() => {
+    cy.visit('localhost:3000')
+  })
+
+  it("There is products on the page", () => {
+    cy.get(".products article").should("be.visible");
+  });
+
+  it("There is 2 products on the page", () => {
+    cy.get(".products article").should("have.length", 2);
+  }
+  );
+});
